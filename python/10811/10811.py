26de2bb8f5e9db8648064042866a8fd7616d6b2c
seeon4143 <seeon4143@users.noreply.github.com>
2026-03-11 10:57:16 +0900

    Boj 10811 바구니 뒤집기 (python)

---
+++ python/10811/10811.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+N,M=map(int, input().split())
+arr= list(range(1,N+1))
+
+for _ in range(M):
+    i,j=map(int,input().split())
+    arr[i-1:j]=arr[i-1:j][::-1]
+print(*arr)
+    
+    
